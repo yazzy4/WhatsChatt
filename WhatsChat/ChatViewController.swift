09335b3e67b7ab81af61d0d0e58ee3395b601695
yazzy4 <yazzy4@gmail.com>
2019-06-25 17:52:48 -0400

    Creating handle for logout

--- WhatsChat/ChatViewController.swift
+++ WhatsChat/ChatViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
 
+    @objc func handleLogout(){
+        
+    }
 
 }
 
