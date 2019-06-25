0560fba7382c0ffd9fa87041894c988479e76c9a
yazzy4 <yazzy4@gmail.com>
2019-06-25 16:59:18 -0400

    Updating Authentification for email/password

--- WhatsChat/AppDelegate.swift
+++ WhatsChat/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        
         return true
     }
 
