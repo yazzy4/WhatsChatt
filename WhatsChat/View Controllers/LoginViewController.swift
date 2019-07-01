5baec0a1d47fa1cf7fa5877fb9f05fb4d14690a5
yazzy4 <yazzy4@gmail.com>
2019-07-01 17:22:35 -0400

    Setu:p

--- WhatsChat/View Controllers/LoginViewController.swift
+++ WhatsChat/View Controllers/LoginViewController.swift
@@ -13,8 +13,21 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        self.setNeedsStatusBarAppearanceUpdate()
+        
+        let inputContainerView = UIView()
+        inputContainerView.translatesAutoresizingMaskIntoConstraints = false
+        inputContainerView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.addSubview(inputContainerView)
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+        inputContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 
 }
